4b7e548f1e74b230f9fd9b3f57e34d208b63ebc9
gruppjo <jona_blubb@web.de>
2016-10-12 16:27:36 +0200

    initial project setup

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>My first Angular App Bitch</h1>
+  `,
+  styles: [`
+    h1 {
+      color: pink
+    }
+  `]
+})
+export class AppComponent { }

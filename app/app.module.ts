e7c87346f62db47bb88202e12ac43a8ec2626d5e
gruppjo <jona_blubb@web.de>
2016-10-12 18:32:19 +0200

    tab app, add module.id to page @Components, without css/sass

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { AboutPage } from './pages/about/about';
+import { ContactPage } from './pages/contact/contact';
+import { HomePage } from './pages/home/home';
+import { TabsPage } from './pages/tabs/tabs';
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
   declarations: [
-    AppComponent
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
   ],
-  bootstrap: [
-    AppComponent
-  ]
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  providers: []
 })
-
-export class AppModule { }
+export class AppModule {}
---
+++ app/pages/about/about.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  moduleId: module.id,
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}

47a4892a1933c2695d2ed9cde3ef8925a0fbb980
gruppjo <jona_blubb@web.de>
2016-10-26 18:58:44 +0200

    better scss and watch with separate watchers for scssApp and scssOrigin

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,12 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
 var paths = {
-  scss: ['styles.scss', 'app/**/*.scss'],
   scssOrigin: 'styles.scss',
   scssApp: 'app/**/*.scss',
 };
 
-
-// build styles to tmp
-gulp.task('styles', function () {
-
-  // compile css starting from each module's scss
-  return gulp.src(paths.scss, { base: './' })
+gulp.task('scssOrigin', function () {
+  return gulp.src(paths.scssOrigin)
     .pipe($.plumber())
     .pipe($.sourcemaps.init())
     .pipe($.sass.sync({includePaths: ['node_modules/ionicons/dist/scss']}).on('error', $.sass.logError))
@@ -21,10 +16,25 @@ gulp.task('styles', function () {
     .pipe(gulp.dest('./'));
 });
 
+gulp.task('scssApp', function () {
+  return gulp.src(paths.scssApp, { base: './' })
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    .pipe($.sass.sync().on('error', $.sass.logError))
+    .pipe($.sourcemaps.write())
+    .pipe($.autoprefixer({ browsers: ['last 2 versions'], remove: false}))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('scss', gulp.parallel('scssOrigin', 'scssApp'));
+
 var createWatchers = function () {
-  var watcher = gulp.watch(paths.scss);
-  watcher.on('all', gulp.series('styles'));
+  var watchScssOrigin = gulp.watch(paths.scssOrigin);
+  watchScssOrigin.on('all', gulp.series('scssOrigin'));
+
+  var watchScssApp = gulp.watch(paths.scssApp);
+  watchScssApp.on('all', gulp.series('scssApp'));
 };
 
 
-gulp.task('watch', gulp.parallel('styles', createWatchers));
+gulp.task('watch', gulp.parallel('scss', createWatchers));
